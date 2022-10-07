17bc1a35750689e116834a943895aed591155101
Andrew Paynter <andrewpaynter97@gmail.com>
2022-10-07 12:14:36 -0400

    min h screen on transactions page

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -4,7 +4,7 @@ import TransactionChart from '../components/molecules/TransactionChart/Transacti
 
 const Transactions = () => {
   return (
-    <div className='pb-32 pt-16 bg-slate-100'>
+    <div className='pb-32 pt-16 bg-slate-100 min-h-screen'>
       <div className='flex flex-col px-4 sm:px-8 md:px-16 mx-auto md:max-w-screen-md lg:max-w-screen-xl'>
         <Title>Transactions</Title>
         <TransactionChart />

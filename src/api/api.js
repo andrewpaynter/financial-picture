9715320e600723d1e2b4e34469c49a19ac22fe62
Andrew Paynter <andrewpaynter97@gmail.com>
2022-10-07 14:06:57 -0400

    configured environment variable for api uri

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,12 @@
-export default {
-  transactions: 'https://financial-picture-backend.onrender.com/api/transactions',
-  auth: 'https://financial-picture-backend.onrender.com/api/auth',
-  users: 'https://financial-picture-backend.onrender.com/api/users'
+let api_uri = process.env.REACT_APP_API_URI
+const transactions = api_uri + '/api/transactions'
+const auth = api_uri + '/api/auth'
+const users = api_uri + '/api/users'
+
+const api = {
+  transactions: transactions,
+  auth: auth,
+  users: users
 }
+console.log(api)
+export default api

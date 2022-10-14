0363989f6ee816bd1ff78779726add2a660aa4c7
Andrew Paynter <andrewpaynter97@gmail.com>
2022-10-14 16:18:47 -0400

    updated temp features page

--- src/pages/Features.js
+++ src/pages/Features.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import ParagraphText from '../components/atoms/ParagraphText'
 import Title from '../components/atoms/Title'
 
 const Features = () => {
+  let navigate = useNavigate()
   return (
     <div className='flex flex-col content-center text-center px-12'>
-      <Title className='mt-8'>Features</Title>
-      <ParagraphText className='mt-6'>
-        This page is for testing purposes. You probably navigated to this page by clicking on a
-        link. Eventually, all links will point to their relevant pages.
+      <Title className='mt-8'>Oops!</Title>
+      <i className='fa-solid fa-gears fa-2xl mt-8'></i>
+      <ParagraphText className='mt-12'>
+        This page is under construction!{' '}
+        <span
+          onClick={() => navigate('/')}
+          className='hover:cursor-pointer hover:underline hover:text-blue-700'
+        >
+          Click here
+        </span>{' '}
+        to return to the home page.
       </ParagraphText>
     </div>
   )
